2765af174fd5d61eae6f893dd750257e2484ce36
RaulTaquiri <raultaquiri@gmail.com>
2022-06-15 00:31:16 -0300

    Initial commit

---
+++ questao4/src/main/java/com/prova1/questao2/Questao4Application.java
@@ -0,0 +1,13 @@
+package com.prova1.questao2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Questao4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Questao4Application.class, args);
+	}
+
+}
